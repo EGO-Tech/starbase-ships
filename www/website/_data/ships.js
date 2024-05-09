07ed0edc3b36c89bc67da2a1cd39b1e3e4f9b25d
Vincent Teo <vinteo@gmail.com>
2024-05-09 10:50:46 +1000

    website: automate popular for sale ships

--- www/website/_data/ships.js
+++ www/website/_data/ships.js
@@ -1,18 +1,39 @@
 const _ = require('lodash');
 const nconf = require('nconf');
 const { BetaAnalyticsDataClient } = require('@google-analytics/data');
+const { GoogleAuth } = require('google-auth-library');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 require('../config');
 
 if (process.env.NODE_ENV === 'development')
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = nconf.get(
-    'googleAnalytics:credentials'
-  );
-
-const propertyId = nconf.get('googleAnalytics:propertyId');
-const analyticsDataClient = new BetaAnalyticsDataClient();
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = nconf.get('googleCredentials');
 
 async function runReport() {
+  const sheetId = nconf.get('googleSheets:sheetId');
+  const adcAuth = new GoogleAuth({
+    scopes: [
+      'https://www.googleapis.com/auth/spreadsheets',
+      'https://www.googleapis.com/auth/drive.file',
+    ],
+  });
+
+  const doc = new GoogleSpreadsheet(sheetId, adcAuth);
+  await doc.loadInfo();
+
+  const sheet = doc.sheetsByIndex[3];
+  await sheet.loadCells('B2:G3');
+
+  const shipSales = [];
+  for (let i = 1; i < 7; i++) {
+    const { value: name } = sheet.getCell(1, i);
+    const { value: sales } = sheet.getCell(2, i);
+    shipSales.push({ name, sales });
+  }
+
+  const propertyId = nconf.get('googleAnalytics:propertyId');
+  const analyticsDataClient = new BetaAnalyticsDataClient();
+
   const [response] = await analyticsDataClient.runReport({
     property: `properties/${propertyId}`,
     dateRanges: [
@@ -42,13 +63,16 @@ async function runReport() {
     },
   });
 
-  return _(response.rows)
-    .map((row) => ({
-      name: row.dimensionValues[0].value,
-      downloads: +row.metricValues[0].value,
-    }))
-    .orderBy('downloads', ['desc'])
-    .value();
+  return {
+    shipSales: _.orderBy(shipSales, 'sales', ['desc']),
+    shipDownloads: _(response.rows)
+      .map((row) => ({
+        name: row.dimensionValues[0].value,
+        downloads: +row.metricValues[0].value,
+      }))
+      .orderBy('downloads', ['desc'])
+      .value(),
+  };
 }
 
 const shipTypes = [
@@ -71,17 +95,28 @@ const shipTypes = [
 const similarTagsExclude = ['rando-shop'];
 
 module.exports = async (data) => {
-  const shipDownloads = await runReport();
+  const { shipSales, shipDownloads } = await runReport();
   const { ships } = data;
 
-  let popular = [];
+  let popularSale = [];
+  if (!_.isEmpty(shipSales)) {
+    const highest = _.first(shipSales).sales;
+    popularSale = _(shipSales)
+      .filter(({ sales }) => sales > 0.8 * highest)
+      .map('name')
+      .value();
+  }
+
+  let popularFree = [];
   if (!_.isEmpty(shipDownloads)) {
     const highest = _.first(shipDownloads).downloads;
-    popular = _(shipDownloads)
+    popularFree = _(shipDownloads)
       .filter(({ downloads }) => downloads > 0.8 * highest)
       .map('name')
       .value();
   }
+
+  const popular = _.concat(popularSale, popularFree);
 
   _.each(ships, (ship) => {
     ship.popular ??= _.includes(popular, ship.name);
